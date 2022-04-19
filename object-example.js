e9293ce7a1d37fe756dd2e5bd157a02dec7f65a3
daniel <danielglx961@gmail.com>
2022-04-19 19:18:14 +0300

    add object example

---
+++ object-example.js
@@ -0,0 +1,12 @@
+// declration
+let person = {
+    name: "daniel",
+    age: "30",
+    id: "3056011333"
+}
+
+// modify
+person.name = "ronit"
+
+//print
+console.log("hello my name is " + person.name);
