c92546577b0dc6de70128c6a5270d51321da94c3
daniel <danielglx961@gmail.com>
2022-04-19 19:30:58 +0300

    a

---
+++ diffent-between-object-null-undefined.js
@@ -0,0 +1,5 @@
+let o = {} // its an empty object (not null and not undefined)
+let b = [] // its empty array (not null and not undefined)
+let a = null; // its null
+let q; // its undefined 
+let c = undefined // its undefined too
