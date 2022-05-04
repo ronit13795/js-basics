177dc8c302be26395697d0a8dcffb4e3c7928ef1
daniel <danielglx961@gmail.com>
2022-05-04 19:48:04 +0300

    a

---
+++ primitiveVsNonPrimitive+and more.js
@@ -0,0 +1,88 @@
+// primitve by value
+// string
+// number
+// boolean
+
+let a = 1;
+
+
+function fu(num) {
+    return num = num + 10
+
+}
+// console.log(fu(a)); // 11
+// console.log(a); // 1
+// a does not changed beacuse he sended by value
+
+// non primitive by reference
+// object
+// array
+// function
+
+
+// let b = {
+//     name: "daniel"
+// }
+
+// function bu(obj) {
+//     obj.lastName = 'cohen'
+// }
+
+// console.log(b);
+// console.log(bu(b));
+// console.log(b);
+
+
+
+let obj = {
+    name: 'daniel',
+    lastName: 'cohen',
+    age: 30,
+    hobbit: 'learn',
+}
+
+// let a = String(1) // "1"
+// Number("1") // 1
+
+// const l = String(123).length
+
+let keys = Object.keys(obj)
+let entry = Object.entries(obj)
+// console.log('Object.keys(obj)', keys);
+// console.log('Object.entries(obj)', entry);
+
+let c = {
+    name: "daniel"
+}
+
+let d = {
+    age: 30
+}
+
+let f = {
+    lastName: 'cohen'
+}
+
+let obj1 = { ...c, ...d, ...f }
+console.log(obj1);
+
+let arr = [1, 2, 3];
+let arr2 = [4, 5, 6]
+let arr3 = [...arr, ...arr2]
+console.log(arr3);
+
+// Object.assign(c, d);
+// console.log(c);
+// console.log(d);
+// d.age = 60
+// console.log(c);
+
+// for (let i = 0; i < keys.length; i++) {
+//     console.log(obj[keys[i]]);
+
+// }
+
+
+
+
+
